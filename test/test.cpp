9e2d84434f78ab1612d7a3d40dc6b406b5ce9ea6
Xuanchen Jiang <xuanchen.jiang@spreadtrum.com>
2019-05-25 18:00:47 +0800

    add test

---
+++ test/test.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+#include "../main.cpp"
+
+TEST(SampleTest, Add){
+    EXPECT_EQ(5, add(3, 2));
+};
+
+TEST(SampleTest, Sub){
+    EXPECT_EQ(3, sub(10, 8));
+};
+
+
+int main(int argc,char**argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
