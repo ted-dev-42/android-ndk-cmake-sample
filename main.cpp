a76b0dff3ca890889b79f2cfe0eb79f578f95581
Xuanchen Jiang <xuanchen.jiang@spreadtrum.com>
2019-05-25 16:35:02 +0800

    first blood

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b) { return a + b; }
+
+int sub(int a, int b) { return a - b; }
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << add(a, b) << endl;
+    cout << sub(a, b) << endl;
+    return 0;
+}
